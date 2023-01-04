ebf91e9997a5021c26e94a3dc21d896ab36679df
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2023-01-04 15:05:27 +0700

    Фикс стилей
    1. Добавлен крестик для закрытия бургера.
    2. Фикс неправильно применяющегося ховера.

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -5,14 +5,15 @@ import menu from '../MenuItem';
 import s from './Menu.module.scss'
 
 import {AiOutlineMenu} from 'react-icons/ai';
+import {RxCross2} from "react-icons/rx";
 
 const Menu = () => {
     const [isBurger, setIsBurger] = useState(false);
 
     return (
         <>
-            <button onClick={() => setIsBurger(!isBurger)}>
-                <AiOutlineMenu />
+            <button onClick={() => setIsBurger(!isBurger)} className={s.burger}>
+                {!isBurger ? <AiOutlineMenu /> : <RxCross2 />}
             </button>
             {
                 <ul className={ isBurger ? s.active : s.menu }>
