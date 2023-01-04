84ebbbce0edf15408ccc9af9a8f0f893b1044194
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2023-01-04 14:39:46 +0700

    Добавлен скелетон

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,20 +1,24 @@
 import React, {useEffect, useState} from 'react';
 
-import Article from '../../components/Article/Article';
-import Categories from "../../components/UI/Categories/Categories";
+import Article from '../../components/Article/Article'
+import Categories from '../../components/UI/Categories/Categories'
 
 import s from './Articles.module.scss'
 import '../../styles/container.scss'
+import ArticlesSkeleton from "./ArticlesSkeleton"
 
 const Articles = () => {
-    const [articles, setArticles] = useState([]);
-    const [categoryId, setCategoryId] = useState(0);
+    const [articles, setArticles] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [categoryId, setCategoryId] = useState(0)
 
     useEffect(() => {
+        setIsLoading(true)
         fetch(`https://63a7f65f7989ad3286f7dd81.mockapi.io/articles?tagId=${categoryId ? categoryId : ''}`)
             .then((res) => res.json())
             .then((arr) => {
                 setArticles(arr)
+                setIsLoading(false)
             })
     }, [categoryId])
     return (
@@ -22,9 +26,9 @@ const Articles = () => {
             <Categories value={categoryId} onClickCategory={(id) => setCategoryId(id)} />
             <div className={s.articleList}>
                 {
-                    articles.map(post =>(
-                        <Article key={post.id} {...post} />
-                    ))
+                    isLoading ? [new Array(4).map( (_, idx) => <ArticlesSkeleton key={idx} />)]
+                        : articles.map(post =>(<Article key={post.id} {...post} />
+                        ))
                 }
             </div>
         </div>
