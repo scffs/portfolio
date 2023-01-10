1a9229e0e08c68e2d67ebc29884164668b8b07ca
scffs <67464545+scffs@users.noreply.github.com>
2023-01-10 21:31:28 +0700

    Доработка 404

--- src/components/UI/ButtonToHome/ButtonToHome.jsx
+++ src/components/UI/ButtonToHome/ButtonToHome.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 
 import {useNavigate} from 'react-router-dom'
+import './Button.scss'
 
 const ButtonToHome = ({text}) => {
     const navigate = useNavigate()
     return (
         <button
-            style={{
-                display: 'inline-block',
-                margin: '0 auto',
-                color: '#727272',
-                width: '15%',
-                fontSize: '.9rem',
-                padding: '15px',
-                borderRadius: '10px',
-                fontWeight: '700',
-                background: '#191b21'
-            }}
+            className='home-btn'
             onClick={() => navigate('/')}
+
         >
             {text}
         </button>
