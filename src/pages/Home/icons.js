b7fc0b4619e223ffbf0d611ac616ee79bbabf9f2
scffs <67464545+scffs@users.noreply.github.com>
2023-01-16 11:46:33 +0700

    Create icons.js

---
+++ src/pages/Home/icons.js
@@ -0,0 +1,41 @@
+import gh from '../../assets/img/icons/gh-light.png'
+import python from '../../assets/img/icons/python.png'
+import node from '../../assets/img/icons/nodejs.png'
+import bs from '../../assets/img/icons/bs.png'
+import c from '../../assets/img/icons/c#.png'
+import ts from '../../assets/img/icons/ts.png'
+import react from '../../assets/img/icons/react.png'
+
+
+const icons = [
+    {
+        src: `${gh}`,
+        alt: 'GitHub'
+    },
+    {
+        src: `${python}`,
+        alt: 'Python'
+    },
+    {
+        src: `${node}`,
+        alt: 'NodeJS'
+    },
+    {
+        src: `${react}`,
+        alt: 'React'
+    },
+    {
+        src: `${bs}`,
+        alt: 'Bootstrap'
+    },
+    {
+        src: `${c}`,
+        alt: 'C#'
+    },
+    {
+        src: `${ts}`,
+        alt: 'TypeScript'
+    },
+]
+
+export default icons
