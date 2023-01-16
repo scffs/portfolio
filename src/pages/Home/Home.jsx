fdc3dc0779b163f6b68eaddfb349070a76313cb2
scffs <67464545+scffs@users.noreply.github.com>
2023-01-16 11:46:51 +0700

    Добавлена анимация на главной странице

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
 import Slider from './Slider/Slider'
+import icons from './icons'
+
+import logo from '../../assets/img/ava.jpg'
 
 import s from './Home.module.scss'
+
 const Home = () => {
     return (
         <div className={s.info}>
             <Slider />
-            <div className="avatar">
-                ad
+            <div className={s.profileImage}>
+                <img className={s.ava} src={logo} alt="ava"/>
+                <div>
+                    <div className={s.circleInner}>
+                        {
+                            icons.map((logo, id) =>(
+                                <img key={id} className={s.logo} src={logo.src} alt={logo.alt}/>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
