2277fed8bd5342251217f4f09a401a2f8a146fe5
scffs <67464545+scffs@users.noreply.github.com>
2023-01-31 08:09:24 +0700

    Update Profile.jsx

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,8 +5,18 @@ import logo from '../../assets/img/ava.jpg'
 import Avatar from '../../components/Avatar/Avatar'
 const Profile = () => {
     return (
-        <div>
+        <div style={{ display: 'flex', gap: '50px'}}>
             <Avatar width='300' height='300' borderRadius='50' src={logo} />
+            <div
+                style={{
+                    backgroundColor: '#2a2a32',
+                    width: '400px',
+                    height: '300px',
+                    borderRadius: '80px 20px',
+                    boxShadow: '0 0 10px 3px rgba(152, 195, 121, .7)'
+            }}
+            >
+            </div>
         </div>
     );
 };
