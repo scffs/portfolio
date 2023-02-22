c9e981b23c6fc104d3cc9970b72dbe8779c4af5d
scffs <67464545+scffs@users.noreply.github.com>
2023-02-22 18:39:20 +0700

    Мелкое исправление

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -11,23 +11,21 @@ import logo from '../../assets/img/ava.webp'
 
 const Article = ({link, preview, title, description, tags, author}) => {
     return (
-        <>
-            <Link to={link} className={s.link} target='_blank' rel="noreferrer">
-                <article className={s.article}>
-                    <img className={s.img} src={preview} alt="img"/>
-                    <div className={s.info}>
-                        <div className={s.top}>
-                            <div className={s.tags}>
-                                <Tag tags={tags}/>
-                            </div>
-                            <Author author={author} authorLogo={logo}/>
+        <Link to={link} className={s.link} target='_blank' rel="noreferrer">
+            <article className={s.article}>
+                <img className={s.img} src={preview} alt="img"/>
+                <div className={s.info}>
+                    <div className={s.top}>
+                        <div className={s.tags}>
+                            <Tag tags={tags}/>
                         </div>
-                        <h3 className={s.h3}>{title}</h3>
-                        <p className={s.description}>{description}</p>
+                        <Author author={author} authorLogo={logo}/>
                     </div>
-                </article>
-            </Link>
-        </>
+                    <h3 className={s.h3}>{title}</h3>
+                    <p className={s.description}>{description}</p>
+                </div>
+            </article>
+        </Link>
     );
 };
 
