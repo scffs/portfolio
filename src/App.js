1602b0efbf4186f366b167132fead0d40b3a657b
scffs <67464545+scffs@users.noreply.github.com>
2023-03-03 17:41:07 +0700

    Little fixes

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ const Profile = lazy(() => import('./pages/Profile/Profile'))
 const Home = lazy(() => import('./pages/Home/Home'))
 
 function App() {
-  return (
-      <Suspense fallback={<div>Загрузка...</div>}>
-          <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/portfolio' element={<Home />} />
-              <Route exact path='/news' element={<Articles />} />
-              <Route path='/profile' element={<Profile />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='*' element={<NotFound />} />
-          </Routes>
-      </Suspense>
-  );
+    return (
+        <Suspense fallback={<div>Загрузка...</div>}>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/portfolio' element={<Home />} />
+                <Route exact path='/news' element={<Articles />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </Suspense>
+    );
 }
 export default App
