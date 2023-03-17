02270ceeec14d5753d9dacd4443907cd28e77f81
scffs <67464545+scffs@users.noreply.github.com>
2023-03-17 15:23:33 +0700

    Updates

---
+++ assets/ButtonToHome-7e5a06df.js
@@ -0,0 +1,1 @@
+import{u as n,j as a}from"./index-1aec8bd4.js";const s=({text:t})=>{const o=n();return a("button",{className:"home-btn",onClick:()=>o("/portfolio"),children:t})};export{s as default};
