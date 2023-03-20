047f4026702964dc40d2f16c38fe65ceea1c9219
scffs <67464545+scffs@users.noreply.github.com>
2023-03-20 15:32:51 +0700

    Updates

--- assets/ButtonToHome-bc3b4ebb.js
+++
@@ -1,1 +0,0 @@
-import{u as a,a as n}from"./index-44039553.js";const s=({text:t})=>{const o=a();return n("button",{className:"home-btn",onClick:()=>o("/portfolio"),children:t})};export{s as default};
--- assets/Icon-c3c7309b.js
+++
@@ -1,1 +0,0 @@
-import"./index-44039553.js";const r=({component:t})=>t;export{r as default};
