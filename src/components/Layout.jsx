c095198e7a1ef676de33019b56d8d02e069c2503
Окулов Семён Михайлович <67464545+scffs@users.noreply.github.com>
2023-04-15 09:51:45 +0700

    format code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,24 +5,24 @@ const Header = lazy(() => import('./Header/Header'));
 const Footer = lazy(() => import('./Footer/Footer'));
 
 const Layout = () => (
-  <div style={
-    {
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-      margin: '0 auto',
-      maxWidth: '1300px',
-      padding: '0 10px',
-    }
-  }>
-    <Header/>
-    <main style={{flex: '1 0 auto'}}>
-      <Suspense>
-        <Outlet/>
-      </Suspense>
-    </main>
-    <Footer/>
-  </div>
+    <div style={
+      {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        margin: '0 auto',
+        maxWidth: '1300px',
+        padding: '0 10px',
+      }
+    }>
+      <Header/>
+      <main style={{flex: '1 0 auto'}}>
+        <Suspense>
+          <Outlet/>
+        </Suspense>
+      </main>
+      <Footer/>
+    </div>
 );
 
 export default Layout;
