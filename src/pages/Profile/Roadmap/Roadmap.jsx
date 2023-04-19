17158ec2092b90424e1062561feedfa5bd4bffee
scffs <67464545+scffs@users.noreply.github.com>
2023-04-19 16:50:03 +0700

    custom height for after element

--- src/pages/Profile/Roadmap/Roadmap.jsx
+++ src/pages/Profile/Roadmap/Roadmap.jsx
@@ -1,34 +1,41 @@
-import React, {lazy, Suspense} from 'react';
-
-import s from './Roadmap.module.scss';
+import React, { lazy, Suspense, useEffect, useRef } from 'react';
 
 import Loading from '../../../components/Loading/Loading.jsx';
 const Skill = lazy(() => import('./Skill/Skill'));
 
 import skills from './Skill/skillsList.js';
 
+import s from './Roadmap.module.scss';
+
 const Roadmap = () => {
-  // TODO: adaptive height of the after element
-  // const refComponent = createRef()
-  // const height = refComponent.current.getBoundingClientRect().height
-  return (
-    <div className={s.content} /*ref={refComponent}*/>
-      <h2 className={s.title}>Education & Experience</h2>
-      <h3 className={s.subTitle}>For more information, look at my <a href="https://github.com/scffs" target="_blank" rel="noreferrer">GitHub</a></h3>
-      <ul className={s.skillsList}>
-        <Suspense fallback={<Loading/>}>
-          {skills.map(({time, skill, knowledge}, id) => (
-            <Skill
-              key={id}
-              time={time}
-              skill={skill}
-              knowledge={knowledge}
-            />
-          ))}
-        </Suspense>
-      </ul>
-    </div>
-  );
+    const refComponent = useRef(null);
+
+    useEffect(() => {
+        if (refComponent.current) {
+            const height = refComponent.current.getBoundingClientRect().height;
+            refComponent.current.style.setProperty('--height-ref', `${height * 11.3}px`);
+        }
+    }, []);
+
+    return (
+        <div className={s.content} ref={refComponent}>
+            <h2 className={s.title}>Education & Experience</h2>
+            <h3 className={s.subTitle}>For more information, look at my <a href="https://github.com/scffs" target="_blank" rel="noreferrer">GitHub</a></h3>
+            <ul className={s.skillsList}>
+                <Suspense fallback={<Loading/>}>
+                    {skills.map(({time, skill, knowledge}, id) => (
+                        <Skill
+                            key={id}
+                            time={time}
+                            skill={skill}
+                            knowledge={knowledge}
+                        />
+                    ))}
+                </Suspense>
+            </ul>
+            <div className={s.after} />
+        </div>
+    );
 };
 
 export default Roadmap;
