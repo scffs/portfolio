6a82434c23211bc68054c4819d402f8d1761813e
scffs <67464545+scffs@users.noreply.github.com>
2023-04-19 13:36:46 +0700

    added rollup/plugin-terser

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import terser from '@rollup/plugin-terser';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/'
-})
+    plugins: [
+        react(),
+        {
+            ...terser({
+                format: {
+                    comments: false,
+                },
+            }),
+            apply: 'build', // only apply this plugin during build
+        },
+    ],
+    build: {
+        target: 'es2018', // specify target for output code
+        polyfillDynamicImport: false, // don't include dynamic import polyfill
+        assetsInlineLimit: 0, // don't inline assets, load them as separate files
+        minify: 'terser', // use the terser plugin to minify code
+    },
+});
