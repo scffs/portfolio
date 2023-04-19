14b1a7b813941ae0ee8f74dc16760ad5b208f41d
scffs <67464545+scffs@users.noreply.github.com>
2023-04-19 12:17:40 +0700

    little code review

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -12,39 +12,43 @@ import './Avatar.scss';
  * @param {number} imageLeft
  */
 
-const Avatar = ({width, height, borderRadius, src, isHexagon, hexagonHeight, imageLeft}) => (
-    <>
-      {isHexagon
-          ? <div className="hexagon"
-                 style={{
-                   width: `${hexagonHeight * 0.645}px`,
-                   height: `${hexagonHeight}px`,
-                   borderRadius: hexagonHeight / 10,
-                 }}
-          >
-            <img style={{
-              position: 'absolute',
-              width: `${width}px`,
-              height: `${height}px`,
-              zIndex: '1',
-              clipPath: 'polygon(51.35% -2.07%, 95.22% 24.72%, 95.16% 76.05%, 50.4% 102.17%, 6.34% 75.5%, 4.25% 24.35%)',
-              borderRadius: '100%',
-              left: `${imageLeft}px`,
-            }}
-                 src={src}
-                 alt='ava'
-                 className='image'
-            />
-          </div>
-          : <img style={{
-            width: `${width}px`,
-            height: `${height}px`,
-            borderRadius: `${borderRadius}`,
-          }}
-                src={src}
-                alt='ava'
-          />}
-    </>
-);
+const Avatar = ({width, height, borderRadius, src, isHexagon, hexagonHeight, imageLeft}) => {
+    const hexagonStyle = isHexagon && {
+        width: `${hexagonHeight * 0.645}px`,
+        height: `${hexagonHeight}px`,
+        borderRadius: `${hexagonHeight / 10}px`,
+        backgroundColor: '#ccc'
+    };
+
+    const imageStyle = isHexagon ? {
+        position: 'absolute',
+        width: `${width}px`,
+        height: `${height}px`,
+        zIndex: '1',
+        clipPath: 'polygon(51.35% -2.07%, 95.22% 24.72%, 95.16% 76.05%, 50.4% 102.17%, 6.34% 75.5%, 4.25% 24.35%)',
+        borderRadius: '100%',
+        left: `${imageLeft}px`,
+    } : {
+        width: `${width}px`,
+        height: `${height}px`,
+        borderRadius: `${borderRadius}`,
+    };
+
+    return (
+        <>
+            {isHexagon
+                ? <div className='hexagon' style={hexagonStyle}>
+                    <img className='image' style={imageStyle}
+                         src={src}
+                         alt='avatar'
+                    />
+                </div>
+                : <img className='image' style={imageStyle}
+                       src={src}
+                       alt='avatar'
+                />}
+        </>
+    );
+};
 
 export default Avatar;
