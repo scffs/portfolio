2dcffe330001c7fc4222f1d41e6cdf43cd3a7de3
scffs <khidey@inbox.ru>
2023-05-01 18:05:55 +0700

    update packages

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,33 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import terser from '@rollup/plugin-terser';
-import { visualizer } from 'rollup-plugin-visualizer';
+import react from '@vitejs/plugin-react-swc';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
     plugins: [
         react(),
-        {
-            ...terser({
-                format: {
-                    comments: false,
-                },
-            }),
-            apply: 'build', // only apply this plugin during build
-        },
-        visualizer({
-            template: 'treemap', // or sunburst
-            open: true,
-            gzipSize: true,
-            brotliSize: true,
-            filename: 'analyze.html',
-        }),
+        // visualizer({
+        //     template: 'treemap', // or sunburst
+        //     open: true,
+        //     gzipSize: true,
+        //     brotliSize: true,
+        //     filename: 'analyze.html',
+        // }),
     ],
     build: {
-        target: 'es2018', // specify target for output code
-        polyfillDynamicImport: false, // don't include dynamic import polyfill
-        assetsInlineLimit: 0, // don't inline assets, load them as separate files
-        minify: 'terser', // use the terser plugin to minify code
-        brotliSize: true,
+        target: 'es2015',
+        polyfillDynamicImport: false,
+        assetsInlineLimit: 0,
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                dead_code: true,
+            },
+            toplevel: false,
+            keep_classnames: false,
+            keep_fnames: false,
+            safari10: false,
+        },
     },
     base: '/portfolio/',
 });
