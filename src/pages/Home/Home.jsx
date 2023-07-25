fdccc5a68cc6dac928087a11f96ea9b7aba863ef
scffs <khidey@inbox.ru>
2023-07-25 23:18:17 +0700

    add ScrollLock

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,49 @@
 import React, { lazy, Suspense } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 import Loading from '../../components/Loading/Loading';
+import ScrollLock from '../../components/ScrollLock';
 const Slider = lazy(() => import('./Slider/Slider'));
 const Avatar = lazy(() => import('../../components/Avatar/Avatar'));
+
 const Icon = lazy(() => import('./Icon/Icon'));
 
 import logo from '../../assets/img/awarding.jpg';
 
-import { iconsList } from './Icon/IconsList.jsx';
+import { iconsList } from './Icon/IconsList';
 
 import s from './Home.module.scss';
 
-const Home = () => (
-  <div className={s.info}>
-    <div className={s.profileImage}>
-      <Suspense fallback={<Loading />}>
-        <Avatar
-          width='280px' height='280px'
-          borderRadius='30px'
-          isHexagon='1'
-          hexagonHeight='280'
-          src={logo}
-        />
-        <div className={s.circleInner}>
-          {iconsList.map(({ component }, id) => (
-            <Icon key={id} component={component}/>
-          ))}
+
+const Home = () => {
+  const isMobile = useMediaQuery({ maxDeviceWidth: 1000 });
+
+  return (
+    <ScrollLock isLocked={!isMobile}>
+      {/* Wrap the content of Home component with ScrollLock */}
+      <div className={s.info}>
+        <div className={s.profileImage}>
+          <Suspense fallback={<Loading />}>
+            <Avatar
+              width='280px' height='280px'
+              borderRadius='30px'
+              isHexagon='1'
+              hexagonHeight='280'
+              src={logo}
+            />
+            <div className={s.circleInner}>
+              {iconsList.map(({ component }, id) => (
+                <Icon key={id} component={component}/>
+              ))}
+            </div>
+          </Suspense>
         </div>
-      </Suspense>
-    </div>
-    <Suspense fallback={<Loading />}>
-      <Slider/>
-    </Suspense>
-  </div>
-);
+        <Suspense fallback={<Loading />}>
+          <Slider/>
+        </Suspense>
+      </div>
+    </ScrollLock>
+  );
+};
 
 export default Home;
