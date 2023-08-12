634c183c260df2fa121cfbfc0ed234e51b73ca38
Semyon Okulov <khidey@inbox.ru>
2023-08-12 13:56:51 +0700

    Updates

--- assets/Category-69291b0f.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-d8d2e843.js";const s=({id:s,tag:t})=>a.jsx("span",{className:`tag tag-${s+1}`,children:t});export{s as default};
---
+++ assets/Category-efd2e6fd.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-14cf6f71.js";const s=({id:s,tag:t})=>a.jsx("span",{className:`tag tag-${s+1}`,children:t});export{s as default};
