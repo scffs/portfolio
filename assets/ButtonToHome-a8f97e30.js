cb4eafdf818af98dfbbf2d6edb014a65c1f0e50f
Semyon Okulov <khidey@inbox.ru>
2023-08-17 12:21:46 +0700

    Updates

--- assets/ButtonToHome-a8f97e30.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-128ba778.js";import{u as t}from"./App-e29d85ba.js";const s=({text:s})=>{const r=t();return o.jsx("button",{className:"home-btn",onClick:()=>r("/portfolio/"),children:s})};export{s as default};
