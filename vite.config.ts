3f69302bddf04ee53e92c9095587c62bfdd0a365
Semyon Okulov <khidey@inbox.ru>
2023-08-20 13:03:30 +0700

    feat(rewrite-VKUI): optimize bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import viteCompression from 'vite-plugin-compression';
-import { viteExternalsPlugin } from "vite-plugin-externals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression(),
-    viteExternalsPlugin({
-      reactDom: 'ReactDOM',
-      lazy: ['React', 'lazy'],
-      VKUI: '@vkontakte/vkui',
-    })
   ],
   build: {
     target: 'es2015',
@@ -27,6 +19,13 @@ export default defineConfig({
       keep_classnames: false,
       keep_fnames: false,
       safari10: false,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
     },
   },
   base: '/portfolio/',
