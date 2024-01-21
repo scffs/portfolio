25394c6438d41e1f6ae763ebd04fc341e09adfb8
Semyon Okulov <khidey@inbox.ru>
2024-01-21 18:35:29 +0700

    Updates

---
+++ assets/vk-gPr3P0fQ.js
@@ -0,0 +1,1 @@
+const s="/portfolio/assets/python-OxKRXtpv.svg",o="/portfolio/assets/react-1mFZYSzV.svg",t="/portfolio/assets/vk-fdVT9e24.svg";export{s as p,o as r,t as v};
--- assets/vk-sj7YOgbU.js
+++
@@ -1,1 +0,0 @@
-const s="/portfolio/assets/react-1mFZYSzV.svg",o="/portfolio/assets/vk-fdVT9e24.svg";export{s as r,o as v};
